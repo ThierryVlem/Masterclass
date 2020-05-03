7f7ece773da1d4ec5932575a196ed724ec5a21fb
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-05-03 14:31:55 +0200

    more added

--- strings2.py
+++ strings2.py
@@ -3,7 +3,21 @@
 #         43210987654321
 parrot = "Norwegian Blue"
 
-print(parrot[0:6])  # Norweg
+print(parrot[0:6:2])  # Nre
+print(parrot[0:6:3])  # Nw
+
+number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
+
+
+
+
+
+"""print(parrot[0:6])  # Norweg
 print(parrot[3:5])
 print(parrot[0:9])
 print(parrot[:9])
@@ -13,8 +27,8 @@ print(parrot[-4:])
 letters = "abcdefghijklmnopqrstuvwxyz"
 
 
-# """print(parrot)
-"""
+print(parrot)
+
 print(parrot[3])
 weWin = 'We win'
 print(weWin[1])
