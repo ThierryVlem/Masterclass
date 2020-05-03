7bc3f5b677b65eeb463f3b8f1645c4689f3540db
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-05-03 14:53:29 +0200

    added sliceback file and slicing backwards..

---
+++ sliceBack.py
@@ -0,0 +1,15 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[25::-1]
+print(backwards)
+a = letters[16:13:-1]
+print(a)
+b = letters[4::-1]
+print(b)
+c = letters[:17:-1]
+print(c)
+
+"""naam = input("Wat is je naam ? : ")
+print("Hey " + naam + ", nu geef ik hem omgekeerd terug!:)")
+backW = naam[::-1]
+print(backW)"""
