2b93b38115b1a3caa2fb40afed062e5ca1cbcfc7
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-05-03 13:06:03 +0200

    slicing

--- strings2.py
+++ strings2.py
@@ -1,17 +1,48 @@
 #
-#         01234567801234
+#         01234567890123
+#         43210987654321
 parrot = "Norwegian Blue"
 
-print(parrot)
+print(parrot[0:6])  # Norweg
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
+print(parrot[10:])
+print(parrot[-4:])
 
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+
+# """print(parrot)
+"""
 print(parrot[3])
-weWin = 'We Win'
+weWin = 'We win'
 print(weWin[1])
 print(weWin[2])
 print(weWin[3])
 print(weWin[4])
 print(weWin[5])
+print()
 
 for elem in weWin[1:6]:
     print(elem)
+
+print()
+
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+"""
 
