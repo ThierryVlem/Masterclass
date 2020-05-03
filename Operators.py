589162a902c09d44358db4a8351624c8d16193fb
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-05-03 11:34:55 +0200

    adding course projects

---
+++ Operators.py
@@ -0,0 +1,12 @@
+a = 12
+b = 3
+
+print(a + b)  # 15
+print(a - b)  # 9
+print(a * b)  # 36
+print(a / b)  # 4.0
+print(a // b) # 4 integer division,rounded down towards minus inin
+print(a % b)  # 0 modulo
+
+print()
+
