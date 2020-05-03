a1c99791a25977df12da275424b00e09d081907a
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-05-03 15:33:17 +0200

    new exercises

---
+++ repFields.py
@@ -0,0 +1,7 @@
+age = 28
+print("My age is " + str(age) + " years")
+print("My age is {0} years".format(age))
+
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
+      .format(31, "jan", "mar", "may", "jul", "aug", "oct", "dec"))
+
---
+++ sequenceOperators.py
@@ -0,0 +1,15 @@
+string1 = "he's "
+string2 = "probably "
+string3 = "pining "
+string4 = "for the "
+string5 = "fjords."
+
+print(string1 + string2 + string3 + string4 + string5)
+
+print("hello " * 5 + str(4))
+
+today = "sunday"
+print("day" in today)
+print("sun" in today)
+print("parrot" in "fjord")
+
