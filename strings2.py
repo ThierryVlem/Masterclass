53761398c215b0e8012dd055fdb7443b88c0fac7
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-05-03 12:31:36 +0200

    for elem loop

--- strings2.py
+++ strings2.py
@@ -5,5 +5,13 @@ parrot = "Norwegian Blue"
 print(parrot)
 
 print(parrot[3])
+weWin = 'We Win'
+print(weWin[1])
+print(weWin[2])
+print(weWin[3])
+print(weWin[4])
+print(weWin[5])
 
+for elem in weWin[1:6]:
+    print(elem)
 
