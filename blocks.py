161270464cec9a50c568605934287be9e7c6b262
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-05-06 21:11:03 +0200

    changed blocks , elif and else

--- blocks.py
+++ blocks.py
@@ -1,15 +1,17 @@
 name = input("Please enter you're name: ")
 age = int(input("How old are you, {0}?: ".format(name)))
 print(age)
-
-if age >= 18:
-    print("You're old enough to vote!")
-    print('Please put an X in the box')
-else:
-    print("Please come back in {0} years {1}".format(18 - age, name))
+# control + /
+# if age >= 18:
+#     print("You're old enough to vote!")
+#     print('Please put an X in the box')
+# else:
+#     print("Please come back in {0} years {1}".format(18 - age, name))
 
 if age < 18:
     print("Please come back in {0} years {1}".format(18 - age, name))
+elif age == 900:
+    print("Soryy, Yoda, you die in return of the yedi!")
 else:
     print("You're old enough to vote!")
     print('Please put an X in the box')
