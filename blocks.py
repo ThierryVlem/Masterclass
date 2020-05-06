db4528f61d28354247d6c72096d57912b58c58f0
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-05-06 20:53:06 +0200

    changes blocks, if statements

--- blocks.py
+++ blocks.py
@@ -1,3 +1,15 @@
-for i in range(1, 13):
-    print("No. {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
-    print("*" * 80)
+name = input("Please enter you're name: ")
+age = int(input("How old are you, {0}?: ".format(name)))
+print(age)
+
+if age >= 18:
+    print("You're old enough to vote!")
+    print('Please put an X in the box')
+else:
+    print("Please come back in {0} years {1}".format(18 - age, name))
+
+if age < 18:
+    print("Please come back in {0} years {1}".format(18 - age, name))
+else:
+    print("You're old enough to vote!")
+    print('Please put an X in the box')
