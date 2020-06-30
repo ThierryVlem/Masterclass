a61bea75cf064fc6f55914e0c41f41c3d99ccc71
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-06-30 19:41:07 +0200

    adding conditions

---
+++ Conditions.py
@@ -0,0 +1,14 @@
+age = int(input("How old are you? "))
+
+# if age >= 16 and age <= 65:
+if 16 <= age <= 65:
+    print("Have a great day at work! :)")
+else:
+    print("You are to young")
+
+print("-" * 80)
+
+if age < 16 or age > 65:
+    print("Enjoy you're free time")
+else:
+    print("Have a great day at work! :)")
