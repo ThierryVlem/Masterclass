7a9d6c8e8535d82e5737453b05cd28087ec79505
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-06-30 20:12:12 +0200

    added truefalse

---
+++ truefalse.py
@@ -0,0 +1,23 @@
+if 0:
+    print("True")
+else:
+    print("False")
+
+name = input("Please enter you're name: ")
+# if name:
+if name != "":
+    print("Are you the person with no name?")
+else:
+    print("Hello , {}".format(name))
+
+
+
+
+# day = "Saturday"
+# temperature = 30
+# raining = True
+#
+# if day == "Saturday" and temperature > 27 or not raining:
+#     print("Go swimming")
+# else:
+#     print("Learn Python")
