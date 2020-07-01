bf92600930e48558a7935fe79fa02f30a15bfd5c
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-07-01 09:24:03 +0200

    added alot

---
+++ forloops.py
@@ -0,0 +1,5 @@
+parrot = "Norwegian Blue"
+
+for character in parrot:
+    print(character)
+# blijft gaan tot parrot op is van letters
--- strings2.py
+++ strings2.py
@@ -1,62 +1,80 @@
 #
 #         01234567890123
 #         43210987654321
-parrot = "Norwegian Blue"
+# parrot = "Norwegian Blue"
+#
+# print(parrot[0:6:2])  # Nre
+# print(parrot[0:6:3])  # Nw
 
-print(parrot[0:6:2])  # Nre
-print(parrot[0:6:3])  # Nw
+# number = "9,223;372:036 854,775;807"
+number = input("Please enter a series of numbers using separators: ")
+separators = ""
+for char in number:
+    if not char.isnumeric():
+        separators = separators + char
 
-number = "9,223;372:036 854,775;807"
-seperators = number[1::4]
-print(seperators)
+print(separators)
 
-values = "".join(char if char not in seperators else " " for char in number).split()
-print([int(val) for val in values])
-
-
-
-
-
-"""print(parrot[0:6])  # Norweg
-print(parrot[3:5])
-print(parrot[0:9])
-print(parrot[:9])
-print(parrot[10:])
-print(parrot[-4:])
-
-letters = "abcdefghijklmnopqrstuvwxyz"
+values = "".join(char if char not in separators else " " for char in number).split()
+print(sum([int(val) for val in values]))
+#
+# quote = """
+# Alright, but apart from the Sanitation, the Medicine, Education, Wine,
+# Public Order, Irrigation, Roads, the Fresh-Water System,
+# and Public Health, what have the Romans ever done for us?
+# """
+#
+# # Use a for loop and an if statement to print just the capitals in the quote above.
+# capitals = ""
+# for char in quote:
+#     if char.isupper():
+#         capitals = capitals + char
+# print(capitals)
 
 
-print(parrot)
 
-print(parrot[3])
-weWin = 'We win'
-print(weWin[1])
-print(weWin[2])
-print(weWin[3])
-print(weWin[4])
-print(weWin[5])
-print()
 
-for elem in weWin[1:6]:
-    print(elem)
 
-print()
-
-print(parrot[3])
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
-
-print()
-
-print(parrot[-11])
-print(parrot[-10])
-print(parrot[-5])
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
-"""
+# """print(parrot[0:6])  # Norweg
+# print(parrot[3:5])
+# print(parrot[0:9])
+# print(parrot[:9])
+# print(parrot[10:])
+# print(parrot[-4:])
+#
+# letters = "abcdefghijklmnopqrstuvwxyz"
+#
+#
+# print(parrot)
+#
+# print(parrot[3])
+# weWin = 'We win'
+# print(weWin[1])
+# print(weWin[2])
+# print(weWin[3])
+# print(weWin[4])
+# print(weWin[5])
+# print()
+#
+# for elem in weWin[1:6]:
+#     print(elem)
+#
+# print()
+#
+# print(parrot[3])
+# print(parrot[4])
+# print(parrot[9])
+# print(parrot[3])
+# print(parrot[6])
+# print(parrot[8])
+#
+# print()
+#
+# print(parrot[-11])
+# print(parrot[-10])
+# print(parrot[-5])
+# print(parrot[-11])
+# print(parrot[-8])
+# print(parrot[-6])
+# """
 
