038238642bbf82b6b9b6a94d788baf75008949c8
Thierry Vleminckx <thierryvleminckx92@gmail.com>
2020-07-05 10:32:18 +0200

    refactor to pep8

--- python_files/EscapeChar.py
+++ python_files/EscapeChar.py
@@ -1,22 +1,22 @@
-splitString = "this string has been\nsplit over\nseveral\nlines"
-print(splitString)
+split_string = "this string has been\nsplit over\nseveral\nlines"
+print(split_string)
 
-tabbedString = "1\t2\t3\t4\t5"
-print(tabbedString)
+tabbed_string = "1\t2\t3\t4\t5"
+print(tabbed_string)
 
 print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
-#or
+# or
 print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
 
 print("""The pet shop owner said "No, no, \
 'e's uh,...he's resting".""")
 
-anotherSplitString ="""This string has been \
+another_split_string = """This string has been \
 split over \
 several \
 lines"""
 
-print(anotherSplitString)
+print(another_split_string)
 
 print("C:\\Users\\timbuchalka\\notes.txt")
 print(r"C:\Users\timbuchalka\notes.txt")
